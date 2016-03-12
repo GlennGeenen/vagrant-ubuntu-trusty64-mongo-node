143b867ff7480b789f718b7dff13248f508ffebb
Glenn Geenen <Glenn Geenen>
2016-03-12 19:29:33 +0100

    update for mongodb 3.2 and nodejs 4.4

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-Vagrant provisioning script for a base box for node+mongodb, built on top of the ubuntu/trusty64 box. 
+Vagrant provisioning script for a base box for node+mongodb, built on top of the ubuntu/trusty64 box.
+
+Installs
+- MongoDB 3.2
+- NodeJS 4.4.x
+- Nginx
+- git
--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -2,9 +2,20 @@
 sudo su -
 
 # http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
-# general dependencies
-apt-get -y update			 
-apt-get -y install nodejs nodejs-legacy npm mongodb-org
+# install git, mongodb and nginx
+apt-get -y update
+apt-get -y install git mongodb-org nginx
+
+# install node version manager
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+
+# pickup changes
+source ~/.profile
+
+# install nodejs lts version
+nvm install 4.4
+nvm use 4.4
+nvm alias default 4.4
